fd28d06b5264803521df618d38a2cfea1492b194
Brenden Ehlers <brenden@theehlers.org>
2019-01-03 17:02:53 -0700

    Adding everything 	modified:   angular.json 	modified:   package-lock.json 	modified:   package.json 	new file:   src/app/about/about.component.html 	renamed:    src/app/app.component.css -> src/app/about/about.component.scss 	new file:   src/app/about/about.component.spec.ts 	new file:   src/app/about/about.component.ts 	modified:   src/app/app.component.html 	new file:   src/app/app.component.scss 	modified:   src/app/app.component.ts 	modified:   src/app/app.module.ts 	new file:   src/app/home/home.component.css 	new file:   src/app/home/home.component.html 	new file:   src/app/home/home.component.spec.ts 	new file:   src/app/home/home.component.ts 	new file:   src/app/page-not-found/page-not-found.component.css 	new file:   src/app/page-not-found/page-not-found.component.html 	new file:   src/app/page-not-found/page-not-found.component.spec.ts 	new file:   src/app/page-not-found/page-not-found.component.ts 	renamed:    src/styles.css -> src/styles.scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { NavbarModule, WavesModule, ButtonsModule } from 'angular-bootstrap-md'
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AboutComponent } from './about/about.component';
+
+const appRoutes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MDBBootstrapModule,
+    NavbarModule,
+    WavesModule,
+    ButtonsModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true }),
+    MatMenuModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
